6fcfd84e8c9a7a0e5adeae47f44a0803a52910bd
Dcasino <d.casino.144662.tc@umindanao.edu.ph>
2025-12-17 11:11:12 +0800

    AddedInventoryClerkAndUpdated

---
+++ Models/models/InventoryClerk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Models.models
+{
+    public class InventoryClerk : User
+    {
+        public InventoryClerk()
+        {
+            Role = "Inventory Clerk";
+        }
+
+        public override List<string> GetPermissions()
+        {
+            return new List<string>
+            {
+                "Manage Stock",
+                "Record Stock In/Out",
+                "Update Products",
+                "Generate Inventory Reports"
+            };
+        }
+
+        public override void ShowDashboard()
+        {
+            // Dashboard logic
+        }
+    }
+}
+
