c649e86bf8c17708406adae908f2f3da7501d8d5
Dcasino <d.casino.144662.tc@umindanao.edu.ph>
2025-12-17 10:05:50 +0800

    AddedStockMovement_AddedInterfaces

--- Models/models/StockMovement.cs
+++ Models/models/StockMovement.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Testing.Models.models
 {
-    internal class StockMovement
+    public class StockMovement
     {
+        public int MovementId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string MovementType { get; set; } // In, Out, Adjustment
+        public int Quantity { get; set; }
+        public DateTime MovementDate { get; set; }
+        public int UserId { get; set; }
+        public string Reference { get; set; }
+        public string Notes { get; set; }
+
+        public StockMovement()
+        {
+            MovementDate = DateTime.Now;
+        }
     }
 }
+
