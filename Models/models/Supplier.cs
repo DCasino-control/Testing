0c0f490081a83d9ae3c435d7f06a995fe133fbc3
Dcasino <d.casino.144662.tc@umindanao.edu.ph>
2025-12-17 09:56:03 +0800

    AddedSupplierMod

---
+++ Models/models/Supplier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Models.models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactPerson { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Supplier()
+        {
+            IsActive = true;
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
+
