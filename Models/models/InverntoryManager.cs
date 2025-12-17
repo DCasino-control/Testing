0f3ba8af55cbb683f6ca78fe58aed4f8d28ee700
Dcasino <d.casino.144662.tc@umindanao.edu.ph>
2025-12-17 11:14:58 +0800

    AdddedInventoryManagerAndUpdated

--- Models/models/InverntoryManager.cs
+++ Models/models/InverntoryManager.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace Testing.Models.models
 {
-    internal class InverntoryManager
+    public class InventoryManager
     {
+        private List<StockMovement> movements = new List<StockMovement>();
+        private int nextId = 1;
+
+        public bool RecordStockIn(Product product, int quantity, string reference, int userId)
+        {
+            if (product.AddStock(quantity))
+            {
+                var movement = new StockMovement
+                {
+                    MovementId = nextId++,
+                    Product = product,
+                    ProductId = product.ProductId,
+                    MovementType = "In",
+                    Quantity = quantity,
+                    UserId = userId,
+                    Reference = reference
+                };
+                movements.Add(movement);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RecordStockOut(Product product, int quantity, string reference, int userId)
+        {
+            if (product.RemoveStock(quantity))
+            {
+                var movement = new StockMovement
+                {
+                    MovementId = nextId++,
+                    Product = product,
+                    ProductId = product.ProductId,
+                    MovementType = "Out",
+                    Quantity = quantity,
+                    UserId = userId,
+                    Reference = reference
+                };
+                movements.Add(movement);
+                return true;
+            }
+            return false;
+        }
+
+        public List<Product> CheckLowStockAlerts(List<Product> products)
+        {
+            return products.Where(p => p.IsLowStock()).ToList();
+        }
+
+        public List<StockMovement> GetMovementHistory()
+        {
+            return movements;
+        }
     }
 }
+
