9453b293a2601831116ec59dc78ce435628707a1
Dcasino <d.casino.144662.tc@umindanao.edu.ph>
2025-12-17 11:19:29 +0800

    AddedAndUpdatedSupplierManager

--- Models/models/SupplierManager.cs
+++ Models/models/SupplierManager.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Testing.Models.models
 {
-    internal class SupplierManager
+    public class SupplierManager
     {
+        private List<Supplier> suppliers = new List<Supplier>();
+        private int nextId = 1;
+
+        public void AddSupplier(Supplier supplier)
+        {
+            supplier.SupplierId = nextId++;
+            suppliers.Add(supplier);
+        }
+
+        public Supplier GetSupplierById(int id)
+        {
+            return suppliers.FirstOrDefault(s => s.SupplierId == id && s.IsActive);
+        }
+
+        public List<Supplier> GetAllSuppliers()
+        {
+            return suppliers.Where(s => s.IsActive).ToList();
+        }
     }
 }
